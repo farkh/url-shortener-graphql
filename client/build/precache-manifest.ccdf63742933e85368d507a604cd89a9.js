91e19f737059e1f22803ca9c9f44624d74e21c78
Ainur Farkh <farkhutdinov1398@gmail.com>
2019-07-28 21:34:12 +0300

    Rebuild client

---
+++ client/build/precache-manifest.ccdf63742933e85368d507a604cd89a9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "658139d19fe8f06c4524dfad8fcbb05c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b24ddf9dcd6f6459a538",
+    "url": "/static/css/2.266e55a5.chunk.css"
+  },
+  {
+    "revision": "f4c5d9eaea1d7c424315",
+    "url": "/static/css/main.fb1fd66f.chunk.css"
+  },
+  {
+    "revision": "b24ddf9dcd6f6459a538",
+    "url": "/static/js/2.1884b325.chunk.js"
+  },
+  {
+    "revision": "f4c5d9eaea1d7c424315",
+    "url": "/static/js/main.39dc8e0b.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
